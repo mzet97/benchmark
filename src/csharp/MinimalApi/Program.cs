55f79001b6fd28abb27b91c40491396bba2eae28
mzet97 <matheus.zeitune.developer@gmail.com>
2026-07-28 18:24:37 -0300

    fix: C# - parse URLs in Program.cs for health checks

--- src/csharp/MinimalApi/Program.cs
+++ src/csharp/MinimalApi/Program.cs
@@ -6,6 +6,37 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Parse DATABASE_URL to Npgsql format
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "";
+if (databaseUrl.StartsWith("postgresql://"))
+{
+    var lastAt = databaseUrl.LastIndexOf('@');
+    var schemeEnd = databaseUrl.IndexOf("://");
+    var userPass = databaseUrl.Substring(schemeEnd + 3, lastAt - schemeEnd - 3);
+    var hostPortDb = databaseUrl.Substring(lastAt + 1);
+    var user = userPass.Split(':')[0];
+    var password = userPass.Contains(':') ? userPass.Substring(userPass.IndexOf(':') + 1) : "";
+    var host = hostPortDb.Split(':')[0];
+    var portDb = hostPortDb.Substring(hostPortDb.IndexOf(':') + 1);
+    var port = portDb.Split('/')[0];
+    var database = portDb.Contains('/') ? portDb.Substring(portDb.IndexOf('/') + 1) : "";
+    var npgsqlConn = $"Host={host};Port={port};Database={database};Username={user};Password={password};Maximum Pool Size=25;Connection Timeout=30";
+    builder.Configuration["ConnectionStrings:DefaultConnection"] = npgsqlConn;
+}
+
+// Parse REDIS_URL to StackExchange.Redis format
+var redisUrl = Environment.GetEnvironmentVariable("REDIS_URL") ?? "";
+if (redisUrl.StartsWith("redis://"))
+{
+    var lastAt = redisUrl.LastIndexOf('@');
+    var schemeEnd = redisUrl.IndexOf("://");
+    var password = redisUrl.Substring(schemeEnd + 4, lastAt - schemeEnd - 4);
+    var hostPort = redisUrl.Substring(lastAt + 1);
+    var host = hostPort.Split(':')[0];
+    var port = hostPort.Contains(':') ? hostPort.Split(':')[1] : "6379";
+    builder.Configuration["Redis:ConnectionString"] = $"{host}:{port},password={password},abortConnect=false";
+}
+
 // Add services to the container
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
